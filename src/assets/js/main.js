763b2ff27d9bfb46e792537cbe6b6c89fcf4c089
mu2na <msdmunna77@gmail.com>
2021-12-21 19:37:57 +0600

    smm first project update

---
+++ src/assets/js/main.js
@@ -0,0 +1,153 @@
+'user strict';
+
+// Preloader
+$(window).on('load', function () {
+    $('.preloader').fadeOut(1500);
+});
+
+
+//Menu Dropdown
+$("ul>li>.sub-menu").parent("li").addClass("has-sub-menu");
+
+$('.menu li a').on('click', function () {
+  var element = $(this).parent('li');
+  if (element.hasClass('open')) {
+    element.removeClass('open');
+    element.find('li').removeClass('open');
+    element.find('ul').slideUp(300, "swing");
+  } else {
+    element.addClass('open');
+    element.children('ul').slideDown(300, "swing");
+    element.siblings('li').children('ul').slideUp(300, "swing");
+    element.siblings('li').removeClass('open');
+    element.siblings('li').find('li').removeClass('open');
+    element.siblings('li').find('ul').slideUp(300, "swing");
+  }
+});
+
+// Responsive Menu
+var headerTrigger = $('.header-trigger');
+headerTrigger.on('click', function(){
+    $('.menu, .header-trigger').toggleClass('active')
+    $('.overlay').toggleClass('active')
+});
+
+
+// Overlay Event
+var over = $('.overlay');
+over.on('click', function() {
+  $('.overlay').removeClass('overlay-color')
+  $('.overlay').removeClass('active')
+  $('.menu, .header-trigger').removeClass('active')
+  $('.header-top').removeClass('active')
+  $('.dashboard__sidebar').removeClass('active')
+})
+
+// Odometer Counter
+$(".counter-item").each(function () {
+  $(this).isInViewport(function (status) {
+    if (status === "entered") {
+      for (var i = 0; i < document.querySelectorAll(".odometer").length; i++) {
+        var el = document.querySelectorAll('.odometer')[i];
+        el.innerHTML = el.getAttribute("data-odometer-final");
+      }
+    }
+  });
+});
+
+// Scroll To Top 
+var scrollTop = $(".scrollToTop");
+$(window).on('scroll', function () {
+  if ($(this).scrollTop() < 500) {
+    scrollTop.removeClass("active");
+  } else {
+    scrollTop.addClass("active");
+  }
+});
+
+//Click event to scroll to top
+$('.scrollToTop').on('click', function () {
+  $('html, body').animate({
+    scrollTop: 0
+  }, 300);
+  return false;
+});
+
+
+$('.testimonial-slider').slick({
+  fade: false,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  infinite: true,
+  autoplay: true,
+  pauseOnHover: true,
+  centerMode: true,
+  dots: true,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+      }
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+      }
+    },
+
+  ]
+});
+
+$('.brand-slider').slick({
+  fade: false,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  infinite: true,
+  autoplay: true,
+  pauseOnHover: true,
+  centerMode: false,
+  dots: false,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 5,
+      }
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 4,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 3,
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+
+  ]
+});
